9c429a07aad5bc413a52734c40962a516f46a9f4
Dakota Mitchell <bikesdakota@gmail.com>
2026-04-20 12:26:56 -0400

    feat/refactor: api changes to enable github pages + render instead of local deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/SDEV255_Final_Project_Group3/',
   server: {
     proxy: {
-      
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
@@ -10,24 +10,30 @@ const app = express();
 require("./config/db");
 
 // Middleware
-app.use(cors({
-    origin: 'http://localhost:5173', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://astrokota.github.io",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
-//Routes
-const courseRoutes = require('./routes/courseRoutes');
-app.use('/api/courses', courseRoutes);
+// Routes
+const courseRoutes = require("./routes/courseRoutes");
+app.use("/api/courses", courseRoutes);
 
-//Test route
-app.get('/', (req, res) => {
-    res.send('Backend server is running');
+// Test route
+app.get("/", (req, res) => {
+  res.send("Backend server is running");
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
